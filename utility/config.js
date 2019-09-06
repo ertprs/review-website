9dd35ce8fd54c54590f83086402c1180015a540f
Mohd <fmohd195@gmail.com>
2019-09-06 18:43:17 +0530

    feature add env variables

--- utility/config.js
+++ utility/config.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://search-api-dev.cryptopolice.com";
+export const baseURL = process.env.NODE_ENV === 'production' ? "https://api.thetrustsearch.com"  : "https://search-api-dev.cryptopolice.com"
 export const screenshotURL="http://api.screenshotlayer.com/api/capture";
 export const faviconURL= "http://www.google.com/s2/favicons";
 export const flagsURL = "https://www.countryflags.io";
