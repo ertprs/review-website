2754b232ab0c9a491ea08fd881331da51cca3d5a
Mohd <fmohd195@gmail.com>
2019-09-18 12:42:46 +0530

    setup redux, add for signup case

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import React from "react";
 import App, { Container } from "next/app";
+import withReduxStore from "../lib/with-redux-store";
+import { Provider } from "react-redux";
 import Head from "next/head";
 
 import { layoutStyles } from "../style";
+
 class MyApp extends App {
   static async getInitialProps({ req, Component, ctx }) {
     let pageProps = {};
@@ -15,7 +18,7 @@ class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, reduxStore} = this.props;
     return (
       <>
         <style jsx>{layoutStyles}</style>
@@ -27,30 +30,38 @@ class MyApp extends App {
             type="text/css"
           />
 
-          { process.env.NODE_ENV === 'production' ? <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          {process.env.NODE_ENV === "production" ? (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-5KPH988');`
-            }}
-          /> : <script></script>}
-          
+              }}
+            />
+          ) : (
+            <script></script>
+          )}
         </Head>
         <Container>
-        {process.env.NODE_ENV === 'production' ?
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KPH988"
+          {process.env.NODE_ENV === "production" ? (
+            <noscript
+              dangerouslySetInnerHTML={{
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KPH988"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-            }}
-          /> : <noscript />}
-          <Component {...pageProps} />
+              }}
+            />
+          ) : (
+            <noscript />
+          )}
+          <Provider store={reduxStore}>
+            <Component {...pageProps} />
+          </Provider>
         </Container>
       </>
     );
   }
 }
 
-export default MyApp;
+export default withReduxStore(MyApp);
