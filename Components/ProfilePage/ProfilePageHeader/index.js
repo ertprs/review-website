118d85319a44f8752b6bfd3363176b9862a220ec
Shubham Chitransh <shubhamchitransh8@gmail.com>
2019-10-07 09:13:56 +0530

    make api call in componentdidmount to get image

--- Components/ProfilePage/ProfilePageHeader/index.js
+++ Components/ProfilePage/ProfilePageHeader/index.js
@@ -5,16 +5,26 @@ import ReviewCard from "../../Widgets/ReviewCard/ReviewCard";
 import RatingIndicators from "../../Widgets/RatingIndicators/RatingIndicators";
 import { profilePageHeaderStyles } from "./profilePageHeaderStyles";
 import _get from "lodash";
-import { connect } from 'react-redux';
-import Placeholder from './headerPlaceholder';
-
+import { connect } from "react-redux";
+import Placeholder from "./headerPlaceholder";
+import axios from "axios";
 class ProfilePageHeader extends Component {
   state = {
-    headerData: {}
+    headerData: {},
+    imageSrc: ""
   };
 
   componentDidMount() {
     // this.setState({ headerData: { ...this.props.headerData } });
+    axios
+      .get("http://localhost:8080/screenshot?domain=https://www.sbilife.co.in/")
+      .then(result => {
+        console.log("result", "result");
+        this.setState({ imageSrc: result.data });
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
   }
 
   // shouldComponentUpdate(nextProps, nextState) {
@@ -30,8 +40,8 @@ class ProfilePageHeader extends Component {
   // }
 
   render() {
-    const { domainProfileData, isLoading } = this.props
-    const headerData = ((domainProfileData || {}).headerData || {}).data || {}
+    const { domainProfileData, isLoading } = this.props;
+    const headerData = ((domainProfileData || {}).headerData || {}).data || {};
     const ratings = (headerData || {}).rating || 0;
     const domain_name = (headerData || {}).domain_name || "";
     const is_verified = (headerData || {}).is_verified || false;
@@ -47,12 +57,14 @@ class ProfilePageHeader extends Component {
         widgetSpacings="2px"
       />
     );
-    return (
-      isLoading ? <div className="row">
+    return isLoading ? (
+      <div className="row">
         <div className="col-md-12">
           <Placeholder />
         </div>
-      </div> : (<Paper paperStyles={{ padding: "5px 0 5px 0" }}>
+      </div>
+    ) : (
+      <Paper paperStyles={{ padding: "5px 0 5px 0" }}>
         <div className="profilePageHeaderContainer">
           <style jsx>{profilePageHeaderStyles}</style>
           <div className="container ">
@@ -63,9 +75,9 @@ class ProfilePageHeader extends Component {
                   image={`https://api.screenshotlayer.com/api/capture?access_key=1ed89e56fa17fe2bd7cc86f2a0e6a209&url=https://www.${domain_name}&viewport=1440x900&width=250&random=${Math.floor(
                     Math.random() * 10 + 1
                   )}`}
+                  // image={`http://localhost:3000/upload?domain=https://www.${domain_name}/`}
                   imgContainerStyles={{
-                    maxWidth: "300px",
-
+                    maxWidth: "300px"
                   }}
                   title={domain_name}
                   subTitle={
@@ -86,7 +98,8 @@ class ProfilePageHeader extends Component {
                   <Card>
                     <div className="companyLink">
                       <a href={`https://www.${domain_name}`} target="_blank">
-                        <i className="fa fa-share-square-o"></i>{domain_name}
+                        <i className="fa fa-share-square-o"></i>
+                        {domain_name}
                       </a>
                     </div>
                     <div>Visit this website</div>
@@ -101,11 +114,11 @@ class ProfilePageHeader extends Component {
                           style={{ color: "green" }}
                         ></i>
                       ) : (
-                          <i
-                            className="fa fa-warning"
-                            style={{ color: "#fcaf16" }}
-                          ></i>
-                        )}
+                        <i
+                          className="fa fa-warning"
+                          style={{ color: "#fcaf16" }}
+                        ></i>
+                      )}
                       <span className="claimed">
                         {is_verified ? "Verified" : "Unverified"}
                       </span>
@@ -118,18 +131,19 @@ class ProfilePageHeader extends Component {
                   </Card>
                 </div>
               </div>
+              <img src={this.state.imageSrc} />
             </div>
           </div>
         </div>
-      </Paper>)
+      </Paper>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { profileData } = state
-  const { domainProfileData, isLoading } = profileData
-  return { domainProfileData, isLoading }
-}
+  const { profileData } = state;
+  const { domainProfileData, isLoading } = profileData;
+  return { domainProfileData, isLoading };
+};
 
-export default connect(mapStateToProps)(ProfilePageHeader)
+export default connect(mapStateToProps)(ProfilePageHeader);
