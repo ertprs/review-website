4245a3762728f4c97b2fcade1b90b0f2cbce5b9c
Shubham Chitransh <shubhamchitransh8@gmail.com>
2019-10-10 16:33:18 +0530

    added reviews in dashboard

--- Components/Widgets/MyReviewsBusiness/myReviewsStyles.js
+++ Components/Widgets/MyReviewsBusiness/myReviewsStyles.js
@@ -1,122 +1,128 @@
 import css from "styled-jsx/css";
 
 export const reviewListStyles = css`
-    .reviewCard {
-        border: 1px solid #f3f3f3;
-        box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
-        margin: 10px 0px;
-        background-color: #fff;
-        border-radius: 5px;
-    }
+  .reviewCard {
+    border: 1px solid #d8d8d8;
+    // box-shadow: 0 1px 2px #d8d8d8;
+    margin: 20px 0px;
+    background-color: #fff;
+    border-radius: 5px;
+  }
 
-    .topBox {
-        border-bottom: 1px solid #f1f1f1;
-        padding: 2%;
-        margin: 0;
-    }
+  .topBox {
+    border-bottom: 1px solid #d8d8d8;
+    padding: 2%;
+    margin: 0;
+  }
 
-    .cardLink {
-        font-size: 18px;
-        cursor: pointer;
-        font-weight: 550;
-    }
+  .cardLink {
+    font-size: 18px;
+    cursor: pointer;
+    font-weight: 550;
+  }
 
-    .cardLink:hover {
-        color: #4084bd;
-    }
+  .cardLink:hover {
+    color: #4084bd;
+  }
 
-    .userName {
-        margin-top: 10px;
-    }
+  .userName {
+    margin-top: 10px;
+  }
 
-    .source {
-        margin-top: 10px;
-        color: grey;
-        font-size: 12px;
-    }
+  .source {
+    margin-top: 10px;
+    color: grey;
+    font-size: 12px;
+  }
 
-    .date {
-        font-size: 14px;
-    }
+  .dateContainer {
+    display: flex;
+    justify-content: flex-end;
+  }
 
-    .bottomBox {
-        margin-top: 10px;
-        padding: 0% 11%;
-    }
+  .date {
+    font-size: 14px;
+  }
 
-    .bottomBoxInner {
-        display: flex;
-    }
+  .bottomBox {
+    margin-top: 10px;
+    padding: 0% 11%;
+  }
 
-    .footerLinks {
-        padding: 0px 20px;
-        cursor: pointer;
-    }
+  .bottomBoxInner {
+    display: flex;
+  }
 
-    // .footerLinks::after {
-    //     content: '';
-    //     width: 20%;
-    //     position: absolute;
-    //     top: 100%;
-    //     left: 0;
-    //     display: none;
-    // }
+  .footerLinks {
+    padding: 0px 20px;
+    cursor: pointer;
+  }
 
-    .footerLinks:hover {
-        color: #4084bd;
-        border-bottom: 5px solid #4084bd; 
-    }
+  // .footerLinks::after {
+  //     content: '';
+  //     width: 20%;
+  //     position: absolute;
+  //     top: 100%;
+  //     left: 0;
+  //     display: none;
+  // }
 
-    // .footerLinks:hover.footerLinks::after {
-    //     display: block;
+  .footerLinks:hover {
+    color: #4084bd;
+    border-bottom: 5px solid #4084bd;
+  }
 
-    // }
+  // .footerLinks:hover.footerLinks::after {
+  //     display: block;
 
-    .icons {
-        margin-right: 8px;
-    }
+  // }
 
-    .replyCard {
-        border: 1px solid #f3f3f3;
-        background-color: #f7f7f7;
-        border-radius: 0px 5px 5px 0px;
-    }
+  .icons {
+    margin-right: 8px;
+  }
 
-    .companyName {
-        margin-top: 20px;
-        margin-left: 30px;
-    }
+  .replyCard {
+    border: 1px solid #f3f3f3;
+    background-color: #f7f7f7;
+    border-radius: 0px 5px 5px 0px;
+    transition: 0.5s;
+  }
 
-    .replyInputBox {
-        margin-top: 20px;
-    }
+  .companyName {
+    margin-top: 20px;
+    margin-left: 30px;
+  }
 
-    .postReplyButton {
-        width: auto;
-        padding: 10px;
-        border: 1px solid #28b661;
-        background: #28b661;
-        color: #fff;
-        font-size: 14px;
-        letter-spacing: .5px;
-        font-weight: 400;
-        border-radius: 3px;
-        -webkit-border-radius: 3px;
-        -moz-border-radius: 3px;
-        -ms-border-radius: 3px;
-        -o-border-radius: 3px;
-        cursor: pointer;
-        transition: all 0.4s;
-        margin-bottom: 20px;
-    }
+  .replyInputBox {
+    margin-top: 20px;
+  }
 
-    .postReplyButton:disabled {
-        border: 1px solid #baf0d0;
-        background: #baf0d0;
-      }
+  .postReplyButton {
+    width: auto;
+    padding: 10px;
+    border: 1px solid #28b661;
+    background: #28b661;
+    color: #fff;
+    font-size: 14px;
+    letter-spacing: 0.5px;
+    font-weight: 400;
+    border-radius: 3px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    -ms-border-radius: 3px;
+    -o-border-radius: 3px;
+    cursor: pointer;
+    transition: all 0.4s;
+    margin-bottom: 20px;
+  }
 
-    .postReplyIcon {
-        padding: 0px 8px;
-        font-size: 15px;
-    }
-`
+  .postReplyButton:disabled {
+    border: 1px solid #baf0d0;
+    background: #baf0d0;
+  }
+
+  .postReplyIcon {
+    padding: 0px 8px;
+    font-size: 15px;
+  }
+`;
--- pages/my-reviews-business.js
+++ pages/my-reviews-business.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import ReviewCard from '../Components/Widgets/MyReviewsBusiness/ReviewCard';
+import React, { Component } from "react";
+import ReviewCard from "../Components/Widgets/MyReviewsBusiness/ReviewCard";
 
 class MyReviewsBusiness extends Component {
-    render() {
-        return (
-            <div className="container">
-                <ReviewCard rating={4} />
-                <ReviewCard rating={3} />
-                <ReviewCard rating={4.5} />
-                <ReviewCard rating={3} />
-                <ReviewCard rating={5} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <style jsx>{`
+          .reviewsContainer {
+            margin: 20px 40px;
+          }
+        `}</style>
+        <div className="reviewsContainer">
+          <ReviewCard rating={4} />
+          <ReviewCard rating={3} />
+          <ReviewCard rating={4.5} />
+          <ReviewCard rating={3} />
+          <ReviewCard rating={5} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MyReviewsBusiness;
