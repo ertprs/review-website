271ba3b3286b086a9a2588e9b698ce2e4f7ea01c
Shubham Chitransh <shubhamchitransh8@gmail.com>
2019-10-16 13:36:13 +0530

    added redux logger

--- store/store.js
+++ store/store.js
@@ -2,9 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 import thunkMiddleware from "redux-thunk";
-import { persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 // ACTIONS
 
 // export const serverRenderClock = isServer => dispatch => {
@@ -12,18 +12,18 @@ import storage from 'redux-persist/lib/storage'
 // }
 
 const persistConfig = {
-  key: 'primary',
+  key: "primary",
   storage,
   // whitelist: ['exampleData'] // place to select which state you want to persist
-  blacklist: ['auth', 'trustVote']
-}
+  blacklist: ["auth", "trustVote"]
+};
 
-const persistedReducer = persistReducer(persistConfig, reducers)
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export function initializeStore(initialState = {}) {
   return createStore(
     persistedReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
+    composeWithDevTools(applyMiddleware(thunkMiddleware, logger))
   );
 }
