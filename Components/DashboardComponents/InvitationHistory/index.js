40e74b42e81fffc357f04a84740d050a24dfe610
Shubham Chitransh <shubhamchitransh8@gmail.com>
2019-10-25 15:54:22 +0530

    move creates in first col and mapping status and handle null case in invitation history

--- Components/DashboardComponents/InvitationHistory/index.js
+++ Components/DashboardComponents/InvitationHistory/index.js
@@ -3,27 +3,27 @@ import MaterialTable from "material-table";
 import { connect } from "react-redux";
 import _get from "lodash/get";
 import _isEmpty from "lodash/isEmpty";
-import axios from 'axios';
+import axios from "axios";
 
 const columns = [
-  { title: "Id", field: "id" },
+  { title: "Created", field: "created" },
   { title: "Email", field: "email" },
   { title: "Campaign Name", field: "campaign_name" },
   { title: "Status", field: "status" },
-  { title: "Created", field: "created" },
   { title: "Reference", field: "reference" }
 ];
 
-function createData(
-  customerEmail,
-  status,
-  created,
-  sent,
-  type,
-  referenceNumber
-) {
-  return { customerEmail, status, created, sent, type, referenceNumber };
-}
+const parseTableData = tableData => {
+  let result = tableData.map(data => {
+    let status = _get(data, "status", 0);
+    status = status === null ? 0 : status;
+    let statusInWords = status === 1 ? "Sent" : "Not Sent";
+    let reference = _get(data, "reference", "##");
+    reference = reference === null ? "##" : reference;
+    return { ...data, status: statusInWords, reference };
+  });
+  return result;
+};
 
 class InvitationHistory extends Component {
   render() {
@@ -41,14 +41,18 @@ class InvitationHistory extends Component {
               method: "GET",
               url: url,
               headers: { Authorization: `Bearer ${token}` }
-            })
-            .then(result => {
-                resolve({
-                  data: result.data.invitations,
-                  page: query.page,
-                  totalCount: result.data.total
-                });
+            }).then(result => {
+              let tableData = _get(result, "data.invitations", []);
+              let parsedTableData = [];
+              if (Array.isArray(tableData) && !_isEmpty(tableData)) {
+                parsedTableData = parseTableData(tableData);
+              }
+              resolve({
+                data: parsedTableData,
+                page: query.page,
+                totalCount: result.data.total
               });
+            });
           })
         }
       />
@@ -62,7 +66,7 @@ const mapStateToProps = state => {
   const errroMsg = _get(dashboardData, "transactionHistory.errorMsg", "");
   const isLoading = _get(dashboardData, "transactionHistory.isLoading", false);
   let success = false;
-  const token = _get(auth, "logIn.token", "")
+  const token = _get(auth, "logIn.token", "");
   if (invitations) {
     if (Array.isArray(invitations) && !_isEmpty(invitations)) {
       success = true;
