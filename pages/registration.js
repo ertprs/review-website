bf65f9cdd9a647cbec5ae63ffd6efee66359a5bd
Mohd <fmohd195@gmail.com>
2019-11-05 14:30:58 +0530

    styling issues of wot date, mobile design fixed

--- pages/registration.js
+++ pages/registration.js
@@ -44,7 +44,7 @@ const Registration = () => {
 
   return (
     <Layout>
-      <div className="mainContainer">
+      <div className="regContainer">
         <div className="container">
           <div className="col-md-6 offset-md-3">
             <style jsx> {authenticationPageStyles} </style>{" "}
