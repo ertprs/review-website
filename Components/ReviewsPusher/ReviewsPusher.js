9131c58c1834dbb1476405b945c6620f736b1045
Mohd <fmohd195@gmail.com>
2019-11-25 19:05:22 +0530

    increase pusher timeout on changing multiple direct Review URLs

--- Components/ReviewsPusher/ReviewsPusher.js
+++ Components/ReviewsPusher/ReviewsPusher.js
@@ -2,10 +2,28 @@ import React, { Component } from "react";
 import Pusher from "pusher-js";
 import { setReviewsPusherConnect } from "../../store/actions/dashboardActions";
 import { connect } from "react-redux";
+import _get from 'lodash/get';
 
 class ReviewsPusher extends Component {
-  state = { reviewScrapeResult: {} };
+  state = { reviewScrapeResult: {}, timeOutKey:"" };
   pusherCopy = null;
+
+  componentDidUpdate(prevProps, prevState){
+    // console.log(this.props.type, "TYPEP")
+    if(prevProps.type!==this.props.type){
+      const {timeOutKey} = this.state;
+      if(this.props.type){
+        if(this.props.type==="LOCATE_PLACE_SUCCESS"){
+          clearTimeout(timeOutKey);
+          let newTimeOutKey = setTimeout(()=>{
+            this.pusherCopy.disconnect();
+          }, 300000)
+          // console.log(newTimeOutKey, "NEW_TIMEOUT_KEY");
+          this.setState({timeOutKey:newTimeOutKey})
+        }
+      }
+    }
+  }
 
   componentDidMount() {
     const { domain, setReviewsPusherConnect } = this.props;
@@ -38,9 +56,12 @@ class ReviewsPusher extends Component {
       console.log(data, "response from reviews pusher DomainNameAggregator");
     });
 
-    setTimeout(() => {
+    let timeOutKey = setTimeout(() => {
       pusher.disconnect();
     }, 300000);
+
+    this.setState({timeOutKey});
+    // console.log(timeOutKey, "TIMEOUTKEY");
 
     pusher.connection.bind("disconnected", () => {
       setReviewsPusherConnect(false);
@@ -57,4 +78,9 @@ class ReviewsPusher extends Component {
   }
 }
 
-export default connect(null, { setReviewsPusherConnect })(ReviewsPusher);
+const mapStateToProps = (state, ownProps)=>{
+  const type = _get(state,"dashboardData.type", "")
+  return {type};
+}
+
+export default connect(mapStateToProps, { setReviewsPusherConnect })(ReviewsPusher);
