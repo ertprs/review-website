359af3067637386f6ff780a9cba8b94b58196bad
Mohd <fmohd195@gmail.com>
2019-11-29 11:55:07 +0530

    remove unwanted files

--- Components/Widgets/SearchBox/SearchBox.js
+++ Components/Widgets/SearchBox/SearchBox.js
@@ -1,6 +1,5 @@
 import React from "react";
 import searchBoxStyles from "./searchBoxStyles";
-import AmpFormWrapper from "../../AmpWrappers/AmpFormWrapper";
 
 const renderSearchBox = ({
   value,
@@ -39,44 +38,6 @@ const renderSearchBox = ({
               </div>
             </div>
           </form>
-        </div>
-      );
-    case "thetrustsearchIndex":
-      return (
-        <div>
-          <style jsx>{searchBoxStyles}</style>
-          <AmpFormWrapper
-            onSubmit={e => {
-              e.preventDefault();
-              handleSearchSubmit(value);
-            }}
-            formMethod="get"
-            isXHR={false}
-            submitURL="https://thetrustsearch.com/reviews"
-            formTarget="_top"
-          >
-            <div className="searchBoxContainer">
-              <div className="searchBoxInput">
-                <input
-                  type="text"
-                  placeholder={
-                    placeholder
-                      ? placeholder
-                      : "Enter any website domain for verification (ex format: thetrustsearch.com)"
-                  }
-                  onChange={e => onchange(e, stateMethod)}
-                  value={value}
-                  name="domain"
-                  required
-                  pattern="^[^www.][a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
-                />
-              </div>
-              <input type="hidden" name="amp" value="1" />
-              <div className="searchBtnContainer">
-                <button className="searchBtn">{text ? text : "Search"}</button>
-              </div>
-            </div>
-          </AmpFormWrapper>
         </div>
       );
     default:
