68e24407d2ef60143d53ba874cb08cec049935c2
Mohd <fmohd195@gmail.com>
2019-12-02 12:33:51 +0530

    add ratings badge

---
+++ Components/Widgets/RatingsBadge/ratingsBadgeStyles.js
@@ -0,0 +1,14 @@
+import css from "styled-jsx/css";
+
+export default css`
+    .ratingBadgeContainer{
+        display:inline-block;
+        margin-left:1px;
+        background-color:#FEBE42;
+        color:#fff;
+        padding:1% 10% 3% 10%;
+        border-radius:5px;
+        font-size:0.9rem;
+        font-weight:600;
+    }
+`;
