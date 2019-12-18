e1d2cc60f79386dbe9fb951e6630d48546aacb46
Shubham Chitransh <shubhamchitransh8@gmail.com>
2019-12-18 13:20:56 +0530

    validation of isdomain

--- utility/validate.js
+++ utility/validate.js
@@ -42,7 +42,7 @@ const isPhoneNumber = value => {
 };
 
 const isDomain = value => {
-  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(
+  return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(
     value
   );
 };
