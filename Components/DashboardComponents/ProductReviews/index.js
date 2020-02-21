be61be825ad4baccb1f7e9179f25495b94b03a45
Mohd Faisal <mohd@thetrustsearch.com>
2020-02-21 19:33:21 +0530

    available product platforms init

--- Components/DashboardComponents/ProductReviews/index.js
+++ Components/DashboardComponents/ProductReviews/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 
 // state transition diagram for this component :
 // ProductReviews =>  Already configured  => yes => SetupProductReviews
