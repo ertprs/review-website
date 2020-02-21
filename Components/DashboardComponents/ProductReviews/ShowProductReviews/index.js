af7c1cf3ee0f7b62cd3ed1ac96bbe41d0708742a
Mohd Faisal <mohd@thetrustsearch.com>
2020-02-21 18:44:33 +0530

    product reviews init

---
+++ Components/DashboardComponents/ProductReviews/ShowProductReviews/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ShowProductReviews extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ShowProductReviews;
---
+++ Components/DashboardComponents/ProductReviews/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+// state transition diagram for this component :
+// ProductReviews =>  Already configured  => yes => SetupProductReviews
+//                                           no  => ShowProductReviews
+
+class ProductReviews extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ProductReviews;
