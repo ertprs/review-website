11497d097970a361e28967ddbc1882faf04fa075
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-02-21 11:12:58 +0530

    styling changes in campaign finished

--- Components/DashboardComponents/WhatsappInvitation/QRCodeDialog/CampaignFinishedMsg/index.js
+++ Components/DashboardComponents/WhatsappInvitation/QRCodeDialog/CampaignFinishedMsg/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import _get from "lodash/get";
 import Expansion from "./Expansion";
+import CheckSign from "@material-ui/icons/CloudDoneRounded";
 
 export default function({ result }) {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <style jsx>
         {`
-          .heading,
-          .subHeading {
+          .mb_50 {
             margin-bottom: 50px;
+          }
+          .greenText {
+            color: green;
           }
         `}
       </style>
-      <h2 className="heading">Campaign Finished !</h2>
-      <h6 className="subHeading">Here are the results : </h6>
+      <CheckSign style={{ height: "6em", width: "6em", color: "green" }} />
+      <h2 className="mb_50 greenText">Campaign Finished !</h2>
+      <h6 className="mb_50">Here are the results : </h6>
       <Expansion result={result || {}} />
     </div>
   );
