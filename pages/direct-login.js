2ea30fe2f1d89ce4cfa1657dbcf8e0d60a96ccc4
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-02-25 11:09:06 +0530

    created login-directly page

---
+++ pages/direct-login.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import Router from "next/router";
+import { connect } from "react-redux";
+import { businessLogIn } from "../store/actions/authActions";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import _get from "lodash/get";
+
+class DirectLogin extends Component {
+  static async getInitialProps({ res, query }) {
+    const { token } = query;
+    return { token };
+  }
+
+  componentDidMount() {
+    const { token, businessLogIn, auth } = this.props;
+    const type = _get(auth, "type", "");
+    //? making sure that it will not get called twice
+    if (
+      type !== "BUSINESS_LOGIN_INIT" &&
+      type !== "BUSINESS_LOGIN_SUCCESS" &&
+      type !== "BUSINESS_LOGIN_FAILURE"
+    ) {
+      //? email and password can be anything
+      let reqBody = {
+        email: "businessUser@gmail.com",
+        password: "businessUser",
+        alt: token
+      };
+      businessLogIn(reqBody);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { logIn, logInTemp } = _get(this.props, "auth", {});
+    const isLoginFailed = _get(logInTemp, "isLoginFailed", false);
+    const authorized = _get(logIn, "authorized", false);
+    const error = _get(logInTemp, "error", "Some Error Occurred!");
+    if (this.props.auth !== prevProps.auth) {
+      if (isLoginFailed) {
+        let snackbarMsg = "";
+        snackbarMsg = error;
+        this.setState(
+          {
+            showSnackbar: true,
+            variant: "error",
+            snackbarMsg
+          },
+          () => {
+            Router.push("/login");
+          }
+        );
+      } else if (authorized === true) {
+        this.setState(
+          {
+            showSnackbar: true,
+            variant: "success",
+            snackbarMsg: "Logged in successfully!"
+          },
+          () => {
+            Router.push("/dashboard");
+          }
+        );
+      }
+    }
+  }
+
+  render() {
+    const isLoggingIn = _get(this.props, "auth.logInTemp.isLoggingIn", false);
+    return (
+      <div className="center">
+        <style jsx>{`
+          .center {
+            height: 100vh;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        `}</style>
+        {isLoggingIn ? <CircularProgress color="primary" /> : null}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { auth } = state;
+  return { auth };
+};
+
+export default connect(mapStateToProps, { businessLogIn })(DirectLogin);
