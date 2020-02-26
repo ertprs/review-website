0015c4bac7d3c83b04eaa47aac5d7d9d7452b604
Mohd Faisal <mohd@thetrustsearch.com>
2020-02-26 20:05:23 +0530

    add product management structure and dialogs

--- Components/DashboardComponents/ProductReviews/ShowProductReviews/index.js
+++ Components/DashboardComponents/ProductReviews/ShowProductReviews/index.js
@@ -4,32 +4,51 @@ import ReviewsFilter from "./ReviewsFilter";
 import ProductsFilter from "./ProductsFilter";
 import Button from "@material-ui/core/Button";
 import SettingsIcon from "@material-ui/icons/Settings";
+import ProductManagementDialog from "./ProductManagement/ProductManagementDialog";
 
 class ShowProductReviews extends Component {
+  state = {
+    dialogOpen: false
+  };
+
+  handleDialogClose = () => {
+    this.setState({ dialogOpen: false });
+  };
+
   render() {
+    const { dialogOpen } = this.state;
     return (
-      <div>
-        <div className="row">
-          <div className="col-md-4" style={{ textAlign: "center" }}>
-            <ReviewsFilter />
+      <>
+        <div>
+          <div className="row">
+            <div className="col-md-4" style={{ textAlign: "center" }}>
+              <ReviewsFilter />
+            </div>
+            <div className="col-md-4" style={{ textAlign: "center" }}>
+              <ProductsFilter />
+            </div>
+            <div className="col-md-4" style={{ textAlign: "right" }}>
+              <Button
+                color="primary"
+                variant="contained"
+                endIcon={<SettingsIcon />}
+                onClick={() => {
+                  this.setState({ dialogOpen: true });
+                }}
+              >
+                Setup product reviews
+              </Button>
+            </div>
           </div>
-          <div className="col-md-4" style={{ textAlign: "center" }}>
-            <ProductsFilter />
-          </div>
-          <div className="col-md-4" style={{ textAlign: "right" }}>
-            <Button
-              color="primary"
-              variant="contained"
-              endIcon={<SettingsIcon />}
-            >
-              Setup product reviews
-            </Button>
+          <div style={{ margin: "25px 0 15px 0" }}>
+            <ProductReviewCard />
           </div>
         </div>
-        <div style={{ margin: "25px 0 15px 0" }}>
-          <ProductReviewCard />
-        </div>
-      </div>
+        <ProductManagementDialog
+          open={dialogOpen}
+          handleClose={this.handleDialogClose}
+        />
+      </>
     );
   }
 }
--- Components/DashboardComponents/ProductReviews/index.js
+++ Components/DashboardComponents/ProductReviews/index.js
@@ -10,7 +10,7 @@ class ProductReviews extends Component {
   render() {
     return (
       <div>
-        <SetupProductReviews />
+        <ShowProductReviews />
       </div>
     );
   }
